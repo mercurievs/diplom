13507fc06f2d1fbcfce845059aec355f28bce002
GPTprogger <progger.gpt@gmail.com>
2025-11-03 22:18:48 +0600

    first commit

--- config/support/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class SupportConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'support'
-    verbose_name = "Раздел Поддержка"
-    verbose_name_plural = "Поддержки"
--- config/support/models.py
+++
@@ -1,7 +0,0 @@
-import re
-from django.db import models
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-
-class Profile(models.Model):
